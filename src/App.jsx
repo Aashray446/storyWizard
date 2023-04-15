72622e179c1850afc8d8921ac2056fe1e0f0dddf
Abhinavxox <abhinavxox@gmail.com>
2023-04-15 17:57:55 +0530

    <Added>:[fonts]

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import s1 from "./assets/images/s1.jpeg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App font-poiret_one">
       <nav className="flex justify-between items-center p-4">
         <div className="flex items-center">
           <h1 className="text-2xl text-white font-bold ml-2">Story Wizard</h1>
@@ -17,10 +17,26 @@ function App() {
         </div>
         <div className="col-span-3 text-white">
           <div className="col-span-3 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 w-full text-white px-5">
-          <StoryCard heading="The lion King" content="Lorem ipsum bolayr ito s..." imgSrc={s1}  ></StoryCard>
-          <StoryCard heading="The lion King" content="Lorem ipsum bolayr ito sir..." imgSrc={s1}  ></StoryCard>
-          <StoryCard heading="The lion King" content="Lorem ipsum bolayr ito sir..." imgSrc={s1}  ></StoryCard>
-          <StoryCard heading="The lion King" content="Lorem ipsum bolayr ito sir..." imgSrc={s1}  ></StoryCard>
+            <StoryCard
+              heading="The lion King"
+              content="Lorem ipsum bolayr ito s..."
+              imgSrc={s1}
+            ></StoryCard>
+            <StoryCard
+              heading="The lion King"
+              content="Lorem ipsum bolayr ito sir..."
+              imgSrc={s1}
+            ></StoryCard>
+            <StoryCard
+              heading="The lion King"
+              content="Lorem ipsum bolayr ito sir..."
+              imgSrc={s1}
+            ></StoryCard>
+            <StoryCard
+              heading="The lion King"
+              content="Lorem ipsum bolayr ito sir..."
+              imgSrc={s1}
+            ></StoryCard>
           </div>
         </div>
       </div>
--- src/components/storyCard.jsx
+++ src/components/storyCard.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 
-
 const StoryCard = ({ heading, content, imgSrc }) => {
-    return (
-        <div className="p-4 w rounded-lg">
-        <div className="relative">
-            <img src={imgSrc} className="w-full h-64 rounded-lg" />
+  return (
+    <div className="p-4 w rounded-lg">
+      <div className="relative">
+        <img src={imgSrc} className="w-full h-64 rounded-lg" />
         <div className="absolute card w-full bottom-0 ">
-        <h2 className="text-2xl p-1 font-bold">{heading}</h2>
-        <p className="p-2 truncate line-clamp-2">
-            {content}
-        </p>
+          <h2 className="text-2xl p-1 font-bold">{heading}</h2>
+          <p className="p-2 truncate line-clamp-2">{content}</p>
         </div>
-        </div>
-       
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default StoryCard;
