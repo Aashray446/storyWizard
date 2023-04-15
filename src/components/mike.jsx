1209ae436e72af9ca01a3458e3a2e979b8041371
Abhinavxox <abhinavxox@gmail.com>
2023-04-15 17:13:07 +0530

    <Added>:[Search bar]

--- src/components/mike.jsx
+++ src/components/mike.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import microphone from '../assets/microphone.svg';
+import microphone from "../assets/microphone.svg";
 
 export const Mike = () => {
-    return (
-    <div className="center">
-    <div className="flex justify-center items-center h-48 w-48 rounded-full bg-gradient-to-r from-brilliant-rose to-energy-yellow">
-      <img src={microphone} className="w-28 h-28" />
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex justify-center items-center h-48 w-48 rounded-full bg-gradient-to-r from-brilliant-rose to-energy-yellow">
+        <img src={microphone} className="w-28 h-28" />
+      </div>
+      <div className="relative mt-8">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="bg-white bg-opacity-20 focus:outline-none h-12 px-4 rounded-full w-96 text-white font-bold"
+        />
+      </div>
     </div>
-</div>)
-}
+  );
+};
