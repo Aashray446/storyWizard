fac95f68ccb019f57ca3b1141a55f325adc38fe2
Aashray Katiyar <aashray446@gmail.com>
2023-04-15 14:38:20 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="h-full">
+      some Continer
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+    'martinique': '#2e2b49',
+    'brilliant-rose': '#ee5599',
+    'caribbean-green': '#0cc2ac',
+    'energy-yellow': '#f9d561',
+    'royal-blue': '#3b66f4',
+    'cosmic': '#633457',
+    'comet': '#535580',
+    'japanese-laurel': '#047b0d',
+    'armadillo': '#433a34',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
