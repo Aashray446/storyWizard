c42f15d983d157da005ad199cc39aca4bb916986
Abhinavxox <abhinavxox@gmail.com>
2023-04-15 18:54:01 +0530

    <Modified>:[Css edited]

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,33 @@ import { Mike } from "./components/mike";
 import StoryCard from "./components/storyCard";
 import s1 from "./assets/images/s1.jpeg";
 import s2 from "./assets/images/s2.jpeg";
+import s3 from "./assets/images/s3.jpeg";
 function App() {
   return (
-      <div className="grid lg:grid-cols-5 gap-4 grid-cols-1 font-poiret_one">
-        <div className="col-span-2" style={{ height: "100vh" }}>
-          <Mike></Mike>
-        </div>
-        <div className="col-span-3 text-white">
-          <div className="col-span-3 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 w-full text-white px-5">
-            <StoryCard
-              heading="The lion King"
-              content="Lorem ipsum bolayr ito s..."
-              imgSrc={s1}
-            ></StoryCard>
-            <StoryCard
-              heading="The lion King"
-              content="Lorem ipsum bolayr ito sir..."
-              imgSrc={s1}
-            ></StoryCard>
-            <StoryCard
-              heading="The lion King"
-              content="Lorem ipsum bolayr ito sir..."
-              imgSrc={s1}
-            ></StoryCard>
-            <StoryCard
-              heading="The lion King"
-              content="Lorem ipsum bolayr ito sir..."
-              imgSrc={s1}
-            ></StoryCard>
-          </div>
+    <div className="grid lg:grid-cols-5 gap-4 grid-cols-1 font-poiret_one">
+      <div className="col-span-2" style={{ height: "100vh" }}>
+        <Mike></Mike>
+      </div>
+      <div className="col-span-3 text-white">
+        <div className="col-span-3 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 w-full text-white px-5">
+          <StoryCard
+            heading="The lion King"
+            content="A Lion lay asleep in the forest, his great head resting on his paws..."
+            imgSrc={s1}
+          ></StoryCard>
+          <StoryCard
+            heading="The Ant and the Grasshopper"
+            content="Once upon a time, there lived an ant and a grasshopper in a grassy meadow..."
+            imgSrc={s2}
+          ></StoryCard>
+          <StoryCard
+            heading="The hare and the turtle"
+            content="A Hare was making fun of the Tortoise one day for being so slow..."
+            imgSrc={s3}
+          ></StoryCard>
         </div>
       </div>
+    </div>
   );
 }
 
--- src/components/storyCard.jsx
+++ src/components/storyCard.jsx
@@ -5,9 +5,9 @@ const StoryCard = ({ heading, content, imgSrc }) => {
     <div className="p-4 w rounded-lg overflow-hidden group">
       <div className="relative transition-all duration-500 ease-in-out transform group-hover:scale-110">
         <img src={imgSrc} className="w-full h-64 rounded-lg " />
-        <div className="absolute card w-full bottom-0 ">
+        <div className="absolute card w-full bottom-0 rounded-lg">
           <h2 className="text-2xl p-1 font-bold">{heading}</h2>
-          <p className="p-2 truncate line-clamp-2">{content}</p>
+          <p className="p-2 w-80 truncate line-clamp-3">{content}</p>
         </div>
       </div>
     </div>
