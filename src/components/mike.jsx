366c27eadbb8b57d87a438fb83c1fcc7fcf19d34
Abhinavxox <abhinavxox@gmail.com>
2023-04-15 18:35:02 +0530

    <Added>:[animation to components]

--- src/components/mike.jsx
+++ src/components/mike.jsx
@@ -3,16 +3,28 @@ import microphone from "../assets/microphone.svg";
 
 export const Mike = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="flex justify-center items-center h-48 w-48 rounded-full bg-gradient-to-r from-brilliant-rose to-energy-yellow">
+    <div className="flex flex-col items-center justify-center h-screen overflow-hidden group">
+      <div className="flex justify-center items-center h-48 w-48 rounded-full bg-gradient-to-r from-brilliant-rose to-energy-yellow transition-all duration-500 ease-in-out transform group-hover:scale-110">
         <img src={microphone} className="w-28 h-28" />
       </div>
-      <div className="relative mt-8">
+      <div className="relative mt-8 fkex justify-between">
         <input
           type="text"
           placeholder="Search..."
           className="bg-white bg-opacity-20 focus:outline-none h-12 px-4 rounded-full w-96 text-white font-bold"
         />
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            className="absolute right-4 top-3 w-6 h-6 text-white"
+          >
+            <path
+              d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
+              fill="#fff"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
--- src/components/storyCard.jsx
+++ src/components/storyCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const StoryCard = ({ heading, content, imgSrc }) => {
   return (
-    <div className="p-4 w rounded-lg">
-      <div className="relative">
-        <img src={imgSrc} className="w-full h-64 rounded-lg" />
+    <div className="p-4 w rounded-lg overflow-hidden group">
+      <div className="relative transition-all duration-500 ease-in-out transform group-hover:scale-110">
+        <img src={imgSrc} className="w-full h-64 rounded-lg " />
         <div className="absolute card w-full bottom-0 ">
           <h2 className="text-2xl p-1 font-bold">{heading}</h2>
           <p className="p-2 truncate line-clamp-2">{content}</p>
